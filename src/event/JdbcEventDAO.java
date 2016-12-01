4fc9d5542bf59d87250796ac74f7903a3d20c5dc
Lex Banzon Jr <lexbanzonjr@gmail.com>
2016-11-30 21:48:41 -0800

    Updated findAll query and check for null dates

--- src/event/JdbcEventDAO.java
+++ src/event/JdbcEventDAO.java
@@ -25,16 +25,27 @@ public class JdbcEventDAO extends JdbcDaoSupport implements EventDAO
       event.getEndDateTime()});
   }
 
+  public String checkDate(String date)
+  {
+  	if (date == null)
+  		return null;
+  	if (date.equals("01-01-1900"))
+  		return null;
+		else
+			return date;  		
+  }
+  
   public Event convertRowToEvent(Map row)
   {
     Event event = new Event();
     
     event.setEventId((int)row.get(this.cnEventId));
-    event.setCreateDateTime((String)row.get(this.cnCreateDateTime));
-    event.setEndDateTime((String)row.get(this.cnEndDateTime));
-    event.setEventName((String)row.get(this.cnEventName));
     event.setOwnerId((int)row.get(this.cnOwnerId));
     event.setStartDateTime((String)row.get(this.cnStartDateTime));
+
+    event.setCreateDateTime(checkDate((String)row.get(this.cnCreateDateTime)));
+    event.setEndDateTime(checkDate((String)row.get(this.cnEndDateTime)));
+    event.setEventName(checkDate((String)row.get(this.cnEventName)));
     
     return event;    
   }
@@ -70,7 +81,15 @@ public class JdbcEventDAO extends JdbcDaoSupport implements EventDAO
   
   public List<Event> findAll()
   {
-    String sql = "SELECT * FROM EVENT ORDER BY TO_DATE(STARTDATETIME, 'MM-DD-YYYY') DESC";
+    String sql = "SELECT "
+    		+ "EVENTID, "
+    		+ "CREATEDATETIME, "
+    		+ "EVENTNAME, "
+    		+ "OWNERID, "
+    		+ "case startdatetime when '' then '01-01-1900' else startdatetime end case STARTDATETIME, "
+    		+ "case enddatetime when '' then '01-01-1900' when '--' then '01-01-1900' else startdatetime end case ENDDATETIME "
+    		+ "FROM EVENT "
+    		+ "order by 5 desc";
     
     List<Event> list = new ArrayList<Event>();
     
